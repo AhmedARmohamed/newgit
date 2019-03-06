3fec34d402d8e7df0ccb8d6517a3a1d344ebb828
Ahmed Mohamed <mahmedrashid128.com>
2019-03-06 23:00:16 +0300

    Added Apple.java

---
+++ Apple.java
@@ -0,0 +1,13 @@
+
+package huncho;
+import java.util.Scanner;
+
+public class Apple {
+		public static void main(String[] args) {
+			Scanner input = new Scanner(System.in);
+			Tuna tunaobj = new Tuna("huncho");
+			tunaobj.saying();
+			
+			
+		}
+}
